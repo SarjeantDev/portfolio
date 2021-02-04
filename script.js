9552d93e5d021ccfcdcdd85d30e18e7912c54e58
SarjeantDev <bensarjeant@gmail.com>
2021-02-03 19:15:44 -0500

    just pushing a few changes over the past couple weeks!

--- script.js
+++ script.js
@@ -1,150 +1,69 @@
+// simple hamburger function to display mobile menu
+const navItems = document.getElementById('navItems');
+const hamburgerIco = document.getElementById('hamburger');
 
-// simple function to display nav in a fixed position when user reachs 550px from the top
-scrollFunction = () => {
-    const navElem = document.getElementById('nav').style;
-    let windowWidth = window.innerWidth;
-    if (document.body.scrollTop > 515 || document.documentElement.scrollTop > 515) {
-        navElem.backgroundColor = 'rgba(255, 255, 255, 1)';
-        navElem.color = 'rgba(34, 34, 34, 1)';
-        navElem.position = 'fixed';
-        navElem.margin = 0;
-        navElem.borderBottom = '1px solid rgba(34, 34, 34, 1)';
-    } else {
-        navElem.backgroundColor = 'transparent';
-        navElem.color = 'rgba(255, 255, 255, 1)';
-        navElem.borderBottom = 'transparent';
-        navElem.position = 'static';
-    }
-
-}
-window.addEventListener('wheel', scrollFunction);
-
-window.addEventListener('resize', function() {
-    let windowWidth = window.innerWidth;
-    if (windowWidth > 750) {
-        document.getElementById('navItems').style.display='flex';
-    } else {
-        document.getElementById('navItems').style.display = 'none';
-    }
-
+navItems.addEventListener('mouseup', () => {    
+    hamburgerIco.classList.remove("fa-times")
+    navItems.style.display = 'none';
 })
 
 const hamburger = () => {
-    const navItems = document.getElementById('navItems');
-    const hamburger = document.getElementById('hamburger');
-    const navElem = document.getElementById('nav').style;
-    let windowWidth = window.innerWidth;
-    if (navItems.style.display === "" & windowWidth <= 750) {
-        navItems.style.display = 'flex';
-        hamburger.classList.add("fa-times");
-        navElem.backgroundColor = 'rgba(255, 255, 255, 1)';
-        navElem.color = 'rgba(34, 34, 34, 1)';
-        navElem.position = 'fixed';
-        navElem.margin = 0;
-        navElem.borderBottom = '1px solid rgba(34, 34, 34, 1)';
-    } else if (navItems.style.display === 'flex' & windowWidth <= 750) {
-        navItems.style.display = 'none'
-        hamburger.classList.remove("fa-times");
+    if (hamburgerIco.classList.contains("fa-times")) {
+        hamburgerIco.classList.remove("fa-times");
+        navItems.style.display = 'none';
     } else {
         navItems.style.display = 'flex';
-        hamburger.classList.add("fa-times");
-        navElem.backgroundColor = 'rgba(255, 255, 255, 1)';
-        navElem.color = 'rgba(34, 34, 34, 1)';
-        navElem.position = 'fixed';
-        navElem.margin = 0;
-        navElem.borderBottom = '1px solid rgba(34, 34, 34, 1)';
+        hamburgerIco.classList.add("fa-times");
     }
 }
 
 
-// google maps api code with a dark mode style
-let map;
+function animatedText() {
+    const typedTextSpan = document.querySelector('.emphasisHeader');
+    const textArray = ['Front-End Developer', 'GIS Enthusiast', 'Canoe Tripper'];
+    const typingDelay = 200;
+    const erasingDelay = 100;
+    const newTextDelay = 100;
+    let textArrayIndex = 0;
+    let charIndex = 0;
 
-function initMap() {
-    map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: 43.6795, lng: -79.411 },
-        zoom: 11,
-        disableDefaultUI: true,
-        styles: [
-            { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
-            { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
-            { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
-            {
-                featureType: "administrative.locality",
-                elementType: "labels.text.fill",
-                stylers: [{ color: "#d59563" }],
-            },
-            {
-                featureType: "poi",
-                elementType: "labels.text.fill",
-                stylers: [{ color: "#d59563" }],
-            },
-            {
-                featureType: "poi.park",
-                elementType: "geometry",
-                stylers: [{ color: "#263c3f" }],
-            },
-            {
-                featureType: "poi.park",
-                elementType: "labels.text.fill",
-                stylers: [{ color: "#6b9a76" }],
-            },
-            {
-                featureType: "road",
-                elementType: "geometry",
-                stylers: [{ color: "#38414e" }],
-            },
-            {
-                featureType: "road",
-                elementType: "geometry.stroke",
-                stylers: [{ color: "#212a37" }],
-            },
-            {
-                featureType: "road",
-                elementType: "labels.text.fill",
-                stylers: [{ color: "#9ca5b3" }],
-            },
-            {
-                featureType: "road.highway",
-                elementType: "geometry",
-                stylers: [{ color: "#746855" }],
-            },
-            {
-                featureType: "road.highway",
-                elementType: "geometry.stroke",
-                stylers: [{ color: "#1f2835" }],
-            },
-            {
-                featureType: "road.highway",
-                elementType: "labels.text.fill",
-                stylers: [{ color: "#f3d19c" }],
-            },
-            {
-                featureType: "transit",
-                elementType: "geometry",
-                stylers: [{ color: "#2f3948" }],
-            },
-            {
-                featureType: "transit.station",
-                elementType: "labels.text.fill",
-                stylers: [{ color: "#d59563" }],
-            },
-            {
-                featureType: "water",
-                elementType: "geometry",
-                stylers: [{ color: "#17263c" }],
-            },
-            {
-                featureType: "water",
-                elementType: "labels.text.fill",
-                stylers: [{ color: "#515c6d" }],
-            },
-            {
-                featureType: "water",
-                elementType: "labels.text.stroke",
-                stylers: [{ color: "#17263c" }],
-            }
-        ]
+    function type() {
+        if (charIndex < textArray[textArrayIndex].length) {
+            typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+            charIndex++;
+            setTimeout(type, typingDelay);
+        } else {
+            setTimeout(erase, newTextDelay)
+        }
+    }
 
-    });
-}
+    function erase() {
+        if (charIndex > 0) {
+            typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
+            charIndex--;
+            setTimeout(erase, erasingDelay);
+        }
+        else {
+            textArrayIndex++;
+            if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+            setTimeout(type, typingDelay + 400);
+        }
+    }
+
+    setTimeout(type, newTextDelay);
+};
+
+animatedText();
+
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2FyamVhbnRkZXYiLCJhIjoiY2trb2NiN2dxMDFjaDJvcXNucTd3OWg2dCJ9.me_ws5SdIE5IEUMLXIcncg';
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/dark-v10',
+    center: [-79.396, 43.7], // starting position
+    zoom: 9 // starting zoom
+});
+
+const marker = new mapboxgl.Marker()
+    .setLngLat([-79.396, 43.7])
+    .addTo(map);
